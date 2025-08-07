d92370ac0952aff8807d9ad973b61e5488a536c2
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:30:03 -0300

    Delete somar.py

--- somar.py
+++
@@ -1,7 +0,0 @@
-numero1 = 5
-numero2 = 7
-def somar(numero1,numero2):
-    soma = numero1 + numero2
-    return soma
-resultado = somar(numero1, numero2)
-print("O resultado da soma entre {} e {} é: {}".format(numero1,numero2,resultado))    
