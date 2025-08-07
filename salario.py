e68ccd499ee715ea076ef445bb7a843012ad9e12
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:30:46 -0300

    Delete salario.py

--- salario.py
+++
@@ -1,5 +0,0 @@
-ganho = float(input("Quanto você ganha por hora : "))
-trabalho = float(input("Quantas horas você trabalha no mês : "))
-salario = (ganho * trabalho)*30 
-
-print("Você ganha no mês {}".format(salario)) 
