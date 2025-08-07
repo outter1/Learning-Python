02d8ecbaa9cf5082476aafb43f3aca9549a6cf8d
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:29:04 -0300

    Delete ponderada.py

--- ponderada.py
+++
@@ -1,8 +0,0 @@
-
-media_one = float(input("Digite o valor da primeira média : "))
-media_two = float(input("Digite o valor da segunda média : "))
-media_three = float(input("Digite o valor da terceira média : "))
-media_four = float(input("Digite o valor da quarta média : "))
-media = ((media_one * 1 ) + (media_two * 2) + (media_three * 3 ) + (media_four * 4)) / 10
-
-print("A ponterada entre as notas é {}".format(media))  
