b010e536fadfd50e29fc84c4873b8efc8959feca
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:29:29 -0300

    Delete repeticaoex02.py

--- repeticaoex02.py
+++
@@ -1,9 +0,0 @@
-'''Faça um programa, utilizando while, que mostre na tela de 0 até N, em que
-N é o limite inserido pelo usuário'''
-
-i = 0
-escolha = int(input("Digite quanto aparecera na tela : "))
-
-while i <= escolha:
-    print(i)
-    i += 1 
