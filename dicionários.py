d1f6e44001c84c60cffea9d537743b48e4f65571
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:23:11 -0300

    Delete dicionários.py

--- dicionários.py
+++
@@ -1,6 +0,0 @@
-#Dicionários:
-#Dicio = {chave:valor}
-dicionario = {"Primeiro":1, "Segundo":2, "Terceiro":3}
-dicionario1 = dict(primeiro=1, segundo=2, terceiro=3)
-
-dicionario2 = dict([("primeiro",1), ("segundo",2),("terceiro",3)])
