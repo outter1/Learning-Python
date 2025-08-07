2adfef937b75635e2d7c18d7bf97031fd7e3c108
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:26:08 -0300

    Delete ex1 - revisão.py

--- ex1 - revisão.py
+++
@@ -1,8 +0,0 @@
-pro = input("Digite o seu nome de usuário: ")
-key = input("Digite a sua senha agora: ")
-
-while pro == key:
-    print("Não é aceito senha iguais a usuário")
-    key = input("Digite sua senha novamente: ")
-    
-print("Sucess")
