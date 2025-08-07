f4723cb2cc8c44a2ac75615232475f6ba9966683
Gabriel Silva Bastos <160263825+outter1@users.noreply.github.com>
2025-08-07 14:12:39 -0300

    Delete desenho.py

--- desenho.py
+++
@@ -1,9 +0,0 @@
-'''Crie uma função para desenhar uma linha, usando o caractere '_',
-o tamanho da linha deve ser definido na chamada da função'''
-
-def linha(n):
-    for i in range(n):
-        print(end = '_') #end controla o que é impresso na mensagem ao final dela
-    print("")
-    
-linha(100)
